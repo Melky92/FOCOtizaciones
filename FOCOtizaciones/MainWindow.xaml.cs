8dc4946466602851a7e1b61923eb43171ab4bb91
MELKY <melky92@hotmail.com>
2016-12-20 23:01:27 -0400

    Intento de grilla :v

--- FOCOtizaciones/MainWindow.xaml.cs
+++ FOCOtizaciones/MainWindow.xaml.cs
@@ -18,11 +18,39 @@ namespace FOCOtizaciones
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        
+        private List<Costo> LoadCollectionData()
+        {
+            List<Costo> authors = new List<Costo>();
+            authors.Add(new Costo()
+            {
+                Id = 101,
+                Nombre = "Aplicacion",
+                Precio = 123,
+                Checked = false,
+            });
+            return authors;
+        }
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            dataGridDatos.ItemsSource = LoadCollectionData();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataGridRow dgr = Interfaz.GetRow(dataGridDatos, 0);
+            //DataGridCell dgc = Interfaz.GetCell(dataGridDatos, 0, 3);
+            DataGridCheckBoxColumn dgc = (DataGridCheckBoxColumn)dataGridDatos.GetColumn(3);
+            
+            dgr.IsEnabled = false;
         }
     }
 }
